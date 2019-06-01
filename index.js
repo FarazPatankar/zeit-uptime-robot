0e8dc0bf9419b3e82d15bea586200628908708fd
Faraz Patankar <farazpatankar@gmail.com>
2019-06-01 14:41:30 +0300

    monitor overview + project list

---
+++ index.js
@@ -0,0 +1,67 @@
+const { withUiHook } = require('@zeit/integration-utils');
+
+const {
+  keyForm,
+  projectContainer,
+  monitorOverview,
+} = require('./lib/ui-elements');
+const { setMetadata, resetMetadata } = require('./lib/metadata-helper');
+const { getAccountDetails } = require('./lib/uptime-robot-api');
+const { fetchUserProjects, fetchAliases } = require('./lib/zeit-api');
+const { mapAliasToProjects } = require('./lib/utils');
+
+module.exports = withUiHook(async ({ payload, zeitClient }) => {
+  const { clientState, action, user, team, project, configurationId } = payload;
+  const store = await zeitClient.getMetadata();
+  const ownerSlug = team ? team.slug : user.username;
+  // console.log('payload', payload);
+  // console.log('store', store);
+
+  let contentToRender = ``;
+
+  // Handle new key addition and existing key removal.
+  if (action === 'submit') setMetadata(zeitClient, store, clientState);
+  if (action === 'reset') resetMetadata(zeitClient, store);
+
+  // Check if we have a key and ask for one if we don't.
+  if (!store.uptimeRobotKey) {
+    console.log('No key found. Showing form.');
+    contentToRender = keyForm;
+    return `
+      <Page>
+        <H1>Hey, ${payload.user.name}</H1>
+        ${contentToRender}
+      </Page>
+    `;
+  }
+  console.log('Proceeding as we found a key.');
+
+  // Show the user an overview of their monitors.
+  console.log('Fetching account details');
+  const accountDetails = await getAccountDetails(store.uptimeRobotKey);
+  console.log(accountDetails);
+  contentToRender += monitorOverview(accountDetails);
+  
+  // Check if the user is inside a project. If not, show list of projects.
+  if (!project) {
+    const aliases = await fetchAliases(zeitClient);
+    const projects = await fetchUserProjects(zeitClient);
+    const mappedProjects = mapAliasToProjects(aliases, projects);
+    contentToRender += `
+      <Box>
+        <H2>Projects: </H2>
+        <Box>
+          ${mappedProjects.map(project => `${projectContainer(project, ownerSlug, configurationId)}`)}
+        </Box>
+      </Box>
+    `;
+  }
+
+  return `
+    <Page>
+      <H1>Hey, ${payload.user.name}</H1>
+      <BR />
+      ${contentToRender}
+    </Page>
+  `;
+})
